2644bd18d016bd0c58b0e4d18f694188a49bcd95
jelmerp <jelmerpoelstra@gmail.com>
2023-02-06 21:39:22 -0500

    Add SRA upload code

--- run/setup.sh
+++ run/setup.sh
@@ -14,3 +14,22 @@ cd data/ref/trias
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/025/105/GCF_003025105.1_Trias_v._1.0/GCF_003025105.1_Trias_v._1.0_genomic.gff.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/025/105/GCF_003025105.1_Trias_v._1.0/GCF_003025105.1_Trias_v._1.0_genomic.fna.gz
 gunzip GCF_*
+
+# ==============================================================================
+#                            UPLOAD TO SRA
+# ==============================================================================
+# Move all FASTQ files to one dir
+mkdir -p data/fastq_all
+find data/210430_Pearlly_GSL-PY-2114-transfer -name "*fastq.gz" -exec mv -v {} data/fastq_all/ \;
+
+# Create a dir with all files that need to be uploaded
+mkdir -p data/fastq_ncbi
+cp data/fastq_all/C_BP_[0-9]_* data/fastq_ncbi
+cp data/fastq_all/C_BP_1[0-9]_* data/fastq_ncbi
+cp data/fastq_all/C_BP_20_* data/fastq_ncbi
+
+ftp -i ftp-private.ncbi.nlm.nih.gov # name: subftp / pw: ...
+cd uploads/rawal.27_osu.edu_2nFEStRf
+mkdir jelmer_upload
+cd jelmer_upload
+mput *
